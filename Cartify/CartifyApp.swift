f43e64ee1ade3c4191e888efac9a264bfd42e62f
Soha <sohaelgalyy@gmail.com>
2026-07-20 02:08:24 +0300

    chore(project): initialize Cartify project

---
+++ Cartify/CartifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartifyApp.swift
+//  Cartify
+//
+//  Created by Soha Elgaly on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CartifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cartify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cartify
+//
+//  Created by Soha Elgaly on 20/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
